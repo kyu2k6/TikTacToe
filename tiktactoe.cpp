c4922278ccacc0401cc483b0826f72bdaeaec880
Kevin Yu <xiyangyang2006@gmail.com>
2022-09-16 14:24:16 -0700

    Update tiktactoe.cpp

--- tiktactoe.cpp
+++ tiktactoe.cpp
@@ -2,7 +2,7 @@
 //9-14-22
 //TicTacToe 2 player game
 
-//Cite for refrence: 
+//Cite for refrence:
 //https://stackoverflow.com/questions/48737248/how-do-i-print-a-tic-tac-toe-board-after-the-user-enters-in-the-rows-and-columns
 
 #include <iostream>
@@ -10,29 +10,53 @@
 using namespace std;
 
 void drawBoard();
+bool check_space(char letter, int number, char board[3][3]);
 
 int main() {
-	char play = 'y';
-	while (play == 'y') {
-		drawBoard();
-		cout << "Please enter a coordinate: ";
-	}
-	drawBoard();	
-	return 0;
+        char board[3][3];
+        char play = 'y';
+        //x turn = 0
+        //o turn = 1
+        int turn = 0;
+        char letter;
+        int number;
+        while (play == 'y') {
+                drawBoard();
+                cout << "Please enter a letter: " << endl;
+                cin >> letter;
+                cout << "Please enter a number: " << endl;
+                cin >> number;
+                if (check_space(letter, number, board) == false) {
+                        cout << "Already taken, try again!" << endl;
+                }
+                else {
+                        continue;
+                }
+                play = 'n';
+        }
+        drawBoard();
+        return 0;
 }
 
-bool x_turn() {
-	
+bool check_win() {
+
 }
 
+bool check_space(char letter, int number, char board[3][3]) {
+}
 
 void printNumbers() {
-	cout << "   ";
-	for (int i = 0; i < 3; i++) {
-		cout <<  "   " << i+1 << " ";
-	}
-	cout << endl;
+        cout << "   ";
+        for (int i = 0; i < 3; i++) {
+                cout <<  "   " << i+1 << " ";
+        }
+        cout << endl;
 }
+
+void drawBoard() {
+        printNumbers();
+        for (int i = 0; i < 3; i++) {
+
 
 void drawBoard() {
 	printNumbers();
