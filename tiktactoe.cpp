c8627b0728beab0f90df08099d7b2d88803c30bc
Kevin Yu <xiyangyang2006@gmail.com>
2022-09-20 14:18:06 -0700

    Update tiktactoe.cpp

--- tiktactoe.cpp
+++ tiktactoe.cpp
@@ -9,11 +9,12 @@
 #include <cstring>
 using namespace std;
 
-void drawBoard();
+void drawBoard(int num, int letter);
 bool check_space(char letter, int number, char board[3][3]);
+bool check_win();
 
 int main() {
-        char board[3][3];
+        char board[3][3] = {{ 0 }};
         char play = 'y';
         //x turn = 0
         //o turn = 1
@@ -21,20 +22,26 @@ int main() {
         char letter;
         int number;
         while (play == 'y') {
-                drawBoard();
-                cout << "Please enter a letter: " << endl;
+                cout << "Please enter a letter (a to c): " << endl;
                 cin >> letter;
-                cout << "Please enter a number: " << endl;
+                cout << "Please enter a number (1 to 3): " << endl;
                 cin >> number;
                 if (check_space(letter, number, board) == false) {
                         cout << "Already taken, try again!" << endl;
                 }
                 else {
-                        continue;
+                        if (letter == 'a') {
+                                drawBoard(number, 0);
+                        }
+                        if (letter == 'b') {
+                                drawBoard(number, 1);
+                        }
+                        if (letter == 'c') {
+                                drawBoard(number, 2);
+                        }
                 }
                 play = 'n';
         }
-        drawBoard();
         return 0;
 }
 
@@ -43,6 +50,33 @@ bool check_win() {
 }
 
 bool check_space(char letter, int number, char board[3][3]) {
+        if (letter == 'a') {
+                if (board[0][number] == 1) {
+                        return false;
+                }
+                else {
+                        board[0][number] = 1;
+                        return true;
+                }
+        }
+        if (letter == 'b') {
+                if (board[1][number] == 1) {
+                        return false;
+                }
+                else {
+                        board[1][number] = 1;
+                        return true;
+                }
+        }
+        if (letter == 'c') {
+		if (board[1][number] == 1) {
+                        return false;
+                }
+                else {
+                        board[1][number] = 1;
+                        return true;
+                }
+        }
 }
 
 void printNumbers() {
@@ -53,17 +87,28 @@ void printNumbers() {
         cout << endl;
 }
 
-void drawBoard() {
-	printNumbers();
-	for (int i = 0; i < 3; i++) {
-		cout << " " << char('A'+i);
-		for (int j = 0; j < 3; j++) {
-			cout << "  |  ";
-		}
-		cout << " | " << endl;
-	}
-	cout << endl;
-	
+void drawBoard(int num, int letter) {
+        printNumbers();
+        for (int i = 0; i < 3; i++) {
+                cout << " " << char('A'+i);
+                if (letter == i) {
+                        for (int j = 0; j < 3; j++) {
+                                if (j == num) {
+                                        cout << "  x  ";
+                                }
+                                else {
+                                        cout << "  |  ";
+                                }
+                        }
+                }
+                else {
+                        for (int j = 0; j < 3; j++) {
+                                cout << "  |  ";
+                        }
+                }
+                cout << " | " << endl;
+        }
+        cout << endl;
+
 }
-
-
+                                                                                           
