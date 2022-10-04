96066dee4328e4557d05359f141f2801e5b1d9a7
Kevin Yu <xiyangyang2006@gmail.com>
2022-10-04 14:24:27 -0700

    Update tiktactoe.cpp

--- tiktactoe.cpp
+++ tiktactoe.cpp
@@ -1,6 +1,6 @@
 //Kevin Yu
 //9-14-22
-//TicTacToe 2 player game
+//TicTacToe 2 player gamei
 
 #include <iostream>
 #include <cstring>
@@ -9,10 +9,13 @@
 using namespace std;
 
 void drawBoard(int num, int letter, char board[3][3]);
-bool check_space(char letter, int number, char board[3][3], int &turn);
-bool check_win();
+bool check_space(char letter, int number, char board[3][3], int &turn, int &tie);
+bool check_win(char board[3][3], int &turn);
 
 int main() {
+        int x_wins = 0;
+        int o_wins = 0;
+        int tie = 0;
         char board[3][3] = {{'2','2','2'},{'2','2','2'},{'2','2','2'}};
         char play = 'y';
         //x turn = 0
@@ -25,12 +28,12 @@ int main() {
                 cin >> letter;
                 cout << "Please enter a number (1 to 3): " << endl;
                 cin >> number;
-                if (check_space(letter, number, board, turn) == false) {
+                if (check_space(letter, number, board, turn, tie) == false) {
                         cout << "Already taken, try again!" << endl;
                 }
                 else {
+
                         if (letter == 'a') {
-                                cout << board[0][2] << endl;
                                 drawBoard(number, 0, board);
                         }
                         if (letter == 'b') {
@@ -40,46 +43,140 @@ int main() {
                                 drawBoard(number, 2, board);
                         }
                 }
-                play = 'n';
+
+                if (check_win(board, turn) == true) {
+                        if (turn == 0) {
+                                o_wins += 1;
+                                cout << "O WINS" << endl;
+                        }
+                        else {
+                                x_wins += 1;
+                                cout << "X WINS" << endl;
+                        }
+                        cout << "Play again? Put in y for yes and n for no, " << endl;
+                        cin >> play;
+                        if (play == 'y') {
+                                for (int i = 0; i < 3; i++) {
+                                        for (int j = 0; j < 3; j++) {
+                                                board[i][j] = '2';
+                                        }
+                                }
+                                turn = 0;
+                                tie = 0;
+                        }
+                        else {
+                                cout << "X wins: " << x_wins << ", O wins: " << o_wins << endl;
+                                break;
+                        }
+                }
+                else if (tie == 9) {
+                        cout << "TIE" << endl;
+                        cout << "Play again? Put in y for yes and n for no, " << endl;
+                        cin >> play;
+                        if (play == 'y') {
+                                for (int i = 0; i < 3; i++) {
+                                        for (int j = 0; j < 3; j++) {
+                                                board[i][j] = '2';
+                                        }
+                                }
+                                turn = 0;
+                                tie = 0;
+                        }
+                        else {
+                                cout << "X wins: " << x_wins << ", O wins: " << o_wins << endl;
+                                break;
+                        }
+                }
         }
         return 0;
 }
 
-bool check_win() {
+bool check_win(char board[3][3], int &turn) {
+        int player;
+        if (turn == 0) {
+                player = '1';
+        }
+        else {
+                player = '0';
+        }
+        if ((board[0][0] == player) && (board[0][1] == player) && (board[0][2] == player)) {
+                return true;
+        }
+        if ((board[1][0] == player) && (board[1][1] == player) && (board[1][2] == player)) {
+                return true;
+        }
+        if ((board[2][0] == player) && (board[2][1] == player) && (board[2][2] == player)) {
+                return true;
+        }
+        if ((board[0][0] == player) && (board[1][0] == player) && (board[2][0] == player)) {
+                return true;
+        }
+        if ((board[0][1] == player) && (board[1][1] == player) && (board[2][1] == player)) {
+                return true;
+        }
+        if ((board[0][2] == player) && (board[1][2] == player) && (board[2][2] == player)) {
+                return true;
+        }
+        if ((board[0][0] == player) && (board[1][1] == player) && (board[2][2] == player)) {
+                return true;
+        }
+        if ((board[0][2] == player) && (board[1][1] == player) && (board[2][0] == player)) {
+ }
+
+        return false;
 
 }
 
-bool check_space(char letter, int number, char board[3][3], int &turn) {
+bool check_space(char letter, int number, char board[3][3], int &turn, int &tie) {
         number -= 1;
         if (letter == 'a') {
-                if (board[0][number] == '1' || board[0][number] == '0') {
+                if ((board[0][number] == '1') || (board[0][number] == '0')) {
                         return false;
                 }
-                else
-                        cout << turn << endl;{
-                        board[0][number] = char(turn);
+                else {
+                        if (turn == 1) {
+                                board[0][number] = '1';
+                        }
+                        else {
+                                board[0][number] = '0';
+                        }
+                        tie += 1;
                         turn = abs(turn-1);
+                        return true;
                 }
         }
         if (letter == 'b') {
-                if (board[1][number] == '1' || board[0][number] == '0') {
+                if ((board[1][number] == '1') || (board[1][number] == '0')) {
                         return false;
                 }
                 else {
-                        board[1][number] = char(turn);
+                        if (turn == 1) {
+                                board[1][number] = '1';
+                        }
+                        else {
+                                board[1][number] = '0';
+                        }
+                        tie += 1;
                         turn = abs(turn-1);
+                        return true;
                 }
         }
         if (letter == 'c') {
-                if (board[2][number] == '1' || board[0][number] == '0') {
+                if ((board[2][number] == '1') || (board[2][number] == '0')) {
                         return false;
                 }
                 else {
-                        board[2][number] = char(turn);
+                        if (turn == 1) {
+                                board[2][number] = '1';
+                        }
+                        else {
+                                board[2][number] = '0';
+                        }
+                        tie += 1;
                         turn = abs(turn-1);
+                        return true;
                 }
         }
-        return true;
 }
 
 void printNumbers() {
@@ -95,18 +192,16 @@ void drawBoard(int num, int letter, char board[3][3]) {
         for (int i = 0; i < 3; i++) {
                 cout << char('A'+i);
                 for (int j = 0; j < 3; j++) {
-
-                        cout << "char" << board[i][j];
-                        /*if (board[i][j] == 0) {
+                        if (board[i][j] == '0') {
                                 cout << "X";
                         }
-                        if (board[i][j] == 1) {
+                        if (board[i][j] == '1') {
                                 cout << "O";
                         }
-                        else {
-                                cout << board[i][j];
-                        }*/
+                        if (board[i][j] == '2') {
+                                cout << " ";
+                        }
                 }
                 cout << endl;
         }
-}                                                                                                                         
+}
